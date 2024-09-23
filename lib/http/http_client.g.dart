2db8c749626862091d9a0ab711f58921d803a342
dave murdoch <dave@digitaldementia.com>
2024-09-23 09:40:29 -0500

    upgrade (major) libraries

--- lib/http/http_client.g.dart
+++ lib/http/http_client.g.dart
@@ -6,69 +6,92 @@ part of 'http_client.dart';
 // RetrofitGenerator
 // **************************************************************************
 
-// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers
+// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers,unused_element
 
 class _HttpClient implements HttpClient {
   _HttpClient(
     this._dio, {
     this.baseUrl,
+    this.errorLogger,
   });
 
   final Dio _dio;
 
   String? baseUrl;
 
+  final ParseErrorLogger? errorLogger;
+
   @override
   Future<WantList> getWantList(
-    user,
-    perPage,
-    page,
+    String user,
+    int perPage,
+    int page,
   ) async {
-    const _extra = <String, dynamic>{};
+    final _extra = <String, dynamic>{};
     final queryParameters = <String, dynamic>{
       r'per_page': perPage,
       r'page': page,
     };
     final _headers = <String, dynamic>{};
-    final _data = <String, dynamic>{};
-    final _result =
-        await _dio.fetch<Map<String, dynamic>>(_setStreamType<WantList>(Options(
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<WantList>(Options(
       method: 'GET',
       headers: _headers,
       extra: _extra,
     )
-            .compose(
-              _dio.options,
-              '/users/${user}/wants',
-              queryParameters: queryParameters,
-              data: _data,
-            )
-            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
-    final value = WantList.fromJson(_result.data!);
-    return value;
+        .compose(
+          _dio.options,
+          '/users/${user}/wants',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(
+            baseUrl: _combineBaseUrls(
+          _dio.options.baseUrl,
+          baseUrl,
+        )));
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late WantList _value;
+    try {
+      _value = WantList.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
   }
 
   @override
-  Future<MarketplaceFeed> getMarketplaceFeed(id) async {
-    const _extra = <String, dynamic>{};
+  Future<MarketplaceFeed> getMarketplaceFeed(int id) async {
+    final _extra = <String, dynamic>{};
     final queryParameters = <String, dynamic>{r'release_id': id};
     final _headers = <String, dynamic>{};
-    final _data = <String, dynamic>{};
-    final _result = await _dio
-        .fetch<Map<String, dynamic>>(_setStreamType<MarketplaceFeed>(Options(
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<MarketplaceFeed>(Options(
       method: 'GET',
       headers: _headers,
       extra: _extra,
     )
-            .compose(
-              _dio.options,
-              '/sell/mplistrss',
-              queryParameters: queryParameters,
-              data: _data,
-            )
-            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
-    final value = MarketplaceFeed.fromJson(_result.data!);
-    return value;
+        .compose(
+          _dio.options,
+          '/sell/mplistrss',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(
+            baseUrl: _combineBaseUrls(
+          _dio.options.baseUrl,
+          baseUrl,
+        )));
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late MarketplaceFeed _value;
+    try {
+      _value = MarketplaceFeed.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
   }
 
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
@@ -82,5 +105,22 @@ class _HttpClient implements HttpClient {
       }
     }
     return requestOptions;
+  }
+
+  String _combineBaseUrls(
+    String dioBaseUrl,
+    String? baseUrl,
+  ) {
+    if (baseUrl == null || baseUrl.trim().isEmpty) {
+      return dioBaseUrl;
+    }
+
+    final url = Uri.parse(baseUrl);
+
+    if (url.isAbsolute) {
+      return url.toString();
+    }
+
+    return Uri.parse(dioBaseUrl).resolveUri(url).toString();
   }
 }
