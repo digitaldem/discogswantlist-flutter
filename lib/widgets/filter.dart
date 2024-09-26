36b446203c1b75eac74347c1b5bce84b8828da1a
dave murdoch <dave@digitaldementia.com>
2024-09-25 22:13:28 -0500

    add clear icon on search box, replace default focus purple outline with black

--- lib/widgets/filter.dart
+++ lib/widgets/filter.dart
@@ -11,16 +11,33 @@ class Filter extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     // Dependency on the WantsFilter state
     final value = ref.watch(wantsFilterProvider);
+    final controller = TextEditingController(text: value);
+    controller.value = TextEditingValue(
+      text: value,
+      selection: TextSelection.fromPosition(
+        TextPosition(offset: value.length),
+      ),
+    );
 
     return Padding(
       padding: const EdgeInsets.only(left: 8, right: 8),
       child: TextFormField(
-        decoration: const InputDecoration(
-          border: OutlineInputBorder(),
+        controller: controller,
+        decoration: InputDecoration(
+          border: const OutlineInputBorder(),
+          focusedBorder: const OutlineInputBorder(),
           labelText: 'Search...',
+          labelStyle: const TextStyle(color: Colors.black),
+          floatingLabelStyle: const TextStyle(color: Colors.black),
+          suffixIcon: (value.isNotEmpty)
+              ? IconButton(
+                  onPressed: () => ref.read(wantsFilterProvider.notifier).state = '',
+                  icon: const Icon(Icons.clear),
+                )
+              : null,
         ),
-        onChanged: (value) => ref.read(wantsFilterProvider.notifier).state = value, // Update filter state from user input
-        initialValue: value,
+        onChanged: (newValue) => ref.read(wantsFilterProvider.notifier).state = newValue,
+        enableSuggestions: false,
       ),
     );
   }
